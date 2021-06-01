2bb8ac3c9b1d5801ea792b2fa94b595c41a9041e
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2021-06-01 17:46:12 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,14 +1,26 @@
 import os
-import zipfile
 import numpy as np
-import tensorflow as tf
-
-from tensorflow import keras
-from tensorflow.keras import layers
-
-import nibabel as nib
-from scipy import ndimage
+import logging
+import h5py
+from skimage import transform
+from skimage import util
+from skimage import measure
+import cv2
+from PIL import Image
+import shutil
+import png
+import itertools
+import pydicom # for reading dicom files
+import pandas as pd # for some simple data analysis (right now, just to load in the labels data and quickly reference it)
+import math as mt
+import matplotlib.pyplot as plt
 
 # load data
-
-
+data = read_data.load_and_maybe_process_data(
+        input_folder=config.data_root,
+        preprocessing_folder=config.preprocessing_folder,
+        mode=config.data_mode,
+        size=config.image_size,
+        target_resolution=config.target_resolution,
+        force_overwrite=False
+    )
