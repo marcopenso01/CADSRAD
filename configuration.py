0a8d02f011203283ff726147048a75ed7009758a
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2021-06-07 18:21:28 +0200

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,13 +1,13 @@
 """
 @author: Marco Penso
 """
-import model_structure
 import os
 
 experiment_name = 'prova1'
 
 # Model settings
-model_handle = model_structure.Unet
+model_handle = model_zoo.VGG16model
+#model_handle = model_zoo.ResNet50V2model
 
 # Paths settings
 data_root = '/content/drive/My Drive/Pazienti/train'      
@@ -21,7 +21,13 @@ weights_root = os.path.join(log_root, experiment_name)
 data_mode = '2D'   #2D or 3D
 image_size = (212, 212)    #(212,212) or (116,116,28)  --> (nx,ny,Nz_max) with Nz_max = 0 padding is not applied. This might result in volumes with different Nz
 target_resolution = (1, 1)   #(1.36719, 1.36719) or (2.5,2.5,5)
+nlabels = 2
+
+# Training settings
 split_val_train = 0.2 # between 0.0 and 1.0 and represent the proportion of the dataset to include in the test split.
+batch_size = 8
+learning_rate = 0.001
+
 
 # Pre-process settings
 standardize = False
