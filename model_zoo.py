eaed10c4854d8200269a5bd0518e7cfee6481e45
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2021-06-08 15:54:50 +0200

    Update model_zoo.py

--- model_zoo.py
+++ model_zoo.py
@@ -2,7 +2,8 @@
 @author: Marco Penso
 """
 import tensorflow as tf
-from tensorflow.keras.applications import VGG16
+from keras.layers import *
+import numpy as np
 from tensorflow.keras.applications import InceptionV3
 from tensorflow.keras.applications import ResNet50
 from tensorflow.keras.applications import InceptionResNetV2
@@ -10,19 +11,62 @@ from tensorflow.keras.applications import EfficientNetB0
 from tensorflow.keras.applications import EfficientNetB7
 from tensorflow.keras.applications import ResNet50V2
 
-def VGG16model(images):
-  
-  return VGG16(weights='imagenet', include_top=False)
-  
-  
-def ResNet50V2model(images):
-  
-  return ResNet50V2(weights='imagenet', include_top=False)
-  
 
+def VGG16_model(images, nlabels, mode):
+    
+  from tensorflow.keras.applications import VGG16
+  
+  if mode == '2D':
+    
+  
+  base_model = VGG16(input_shape = images.shape[1:],
+                     include_top = False,
+                     weights = 'imagenet')
+  
+  base_model.trainable = False
+  
+  x = layers.Flatten()(base_model.output)
+  
+  x = layers.Dense(512, init='normal', activation='relu')(x)
+  
+  x = layers.Dropout(0.5)(x)
+  
+  x = layers.Dense(512, init='normal', activation='relu')(x)
+  
+  x = layers.Dropout(0.5)(x)
 
+  x = layers.Dense(nlabels, init='normal', activation='softmax')(x)
 
+  model = tf.keras.models.Model(base_model.input, x, name='VGG16')
+  
+  return model
+  
+  
+def InceptionV3_model(images, nlabels):
+  
+  base_model = InceptionV3(input_shape = images.shape[1:],
+                           include_top = False,
+                           weights = 'imagenet')
+  
+  base_model.trainable = False
+
+  x = layers.Flatten()(base_model.output)
+
+  x = layers.Dense(512, init='normal', activation='relu')(x)
+
+  x = layers.Dropout(0.5)(x)
+
+  x = layers.Dense(512, init='normal', activation='relu')(x)
+
+  x = layers.Dropout(0.5)(x)
+
+  x = layers.Dense(nlabels, init='normal', activation='softmax')(x)
+
+  model = tf.keras.models.Model(base_model.input, x, name='InceptionV3')
+
+  return model
+  
 
 def get_model(images, config):
-  
-  return config.model_handle(images, nlabels=config.nlabels)
+    
+  return config.model_handle(images, nlabels=config.nlabels, mode=config.data_mode)
