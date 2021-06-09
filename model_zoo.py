4f329d57afa6e3e18103f4bb22ad44fbe66bebc8
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2021-06-09 12:53:44 +0200

    Update model_zoo.py

--- model_zoo.py
+++ model_zoo.py
@@ -2,9 +2,10 @@
 @author: Marco Penso
 """
 import tensorflow as tf
-from keras.layers import *
-import numpy as np
-from tensorflow.keras.applications import InceptionV3
+from tensorflow.keras import layers 
+from tensorflow.keras import Model 
+from tensorflow.keras.layers import *
+
 from tensorflow.keras.applications import ResNet50
 from tensorflow.keras.applications import InceptionResNetV2
 from tensorflow.keras.applications import EfficientNetB0
@@ -12,14 +13,26 @@ from tensorflow.keras.applications import EfficientNetB7
 from tensorflow.keras.applications import ResNet50V2
 
 
-def VGG16_model(images, nlabels, mode):
-    
-  from tensorflow.keras.applications import VGG16
-  
+def VGG16_model(input_tensor, nlabels, mode):
+   
   if mode == '2D':
-    
+        
+        if input_tensor.shape[-1] != 1:
+            raise AssertionError('Inadequate input tensor shape. The input must have 1 channels')
+        else:
+            input_tensor_shape = Input(shape=(input_tensor.shape[1], input_tensor.shape[2], 1))
+            images = Concatenate(axis=-1)([input_tensor_shape, input_tensor_shape, input_tensor_shape])
   
-  base_model = VGG16(input_shape = images.shape[1:],
+  if mode == '3D':
+        
+        if input_tensor.shape[-1] != 3:
+            raise AssertionError('Inadequate input tensor shape. The input must have 3 channels')
+        else:
+            images = Input(
+                 
+  from tensorflow.keras.applications import VGG16    
+  
+  base_model = VGG16(input_tensor = images,
                      include_top = False,
                      weights = 'imagenet')
   
@@ -27,44 +40,63 @@ def VGG16_model(images, nlabels, mode):
   
   x = layers.Flatten()(base_model.output)
   
-  x = layers.Dense(512, init='normal', activation='relu')(x)
+  x = layers.Dense(512, kernel_initializer=initializer, activation='relu')(x)
   
   x = layers.Dropout(0.5)(x)
   
-  x = layers.Dense(512, init='normal', activation='relu')(x)
+  x = layers.Dense(512, kernel_initializer=initializer, activation='relu')(x)
   
   x = layers.Dropout(0.5)(x)
 
-  x = layers.Dense(nlabels, init='normal', activation='softmax')(x)
+  output = layers.Dense(nlabels, kernel_initializer=initializer, activation='softmax')(x)
 
-  model = tf.keras.models.Model(base_model.input, x, name='VGG16')
+  model = Model(base_model.input, output, name='VGG16')
   
   return model
   
+
+                
+def InceptionV3_model(input_tensor, nlabels, mode):
+   
+  if mode == '2D':
+        
+        if input_tensor.shape[-1] != 1:
+            raise AssertionError('Inadequate input tensor shape. The input must have 1 channels')
+        else:
+            input_tensor_shape = Input(shape=(input_tensor.shape[1], input_tensor.shape[2], 1))
+            images = Concatenate(axis=-1)([input_tensor_shape, input_tensor_shape, input_tensor_shape])
   
-def InceptionV3_model(images, nlabels):
+  if mode == '3D':
+        
+        if input_tensor.shape[-1] != 3:
+            raise AssertionError('Inadequate input tensor shape. The input must have 3 channels')
+        else:
+            images = Input(
+                 
+  from tensorflow.keras.applications import InceptionV3    
   
-  base_model = InceptionV3(input_shape = images.shape[1:],
+  base_model = InceptionV3(input_tensor = images,
                            include_top = False,
                            weights = 'imagenet')
   
   base_model.trainable = False
-
+  
   x = layers.Flatten()(base_model.output)
-
-  x = layers.Dense(512, init='normal', activation='relu')(x)
-
+  
+  x = layers.Dense(512, kernel_initializer=initializer, activation='relu')(x)
+  
+  x = layers.Dropout(0.5)(x)
+  
+  x = layers.Dense(512, kernel_initializer=initializer, activation='relu')(x)
+  
   x = layers.Dropout(0.5)(x)
 
-  x = layers.Dense(512, init='normal', activation='relu')(x)
+  output = layers.Dense(nlabels, kernel_initializer=initializer, activation='softmax')(x)
 
-  x = layers.Dropout(0.5)(x)
-
-  x = layers.Dense(nlabels, init='normal', activation='softmax')(x)
-
-  model = tf.keras.models.Model(base_model.input, x, name='InceptionV3')
-
+  model = Model(base_model.input, output, name='InceptionV3')
+  
   return model
+                
   
 
 def get_model(images, config):
