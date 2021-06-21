d340c6eb0e1f44b90a5da135e5558adc16f90e52
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2021-06-21 09:04:54 +0200

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -27,7 +27,7 @@ data_root = '/content/drive/My Drive/Pazienti/train'
 test_data_root = '/content/drive/My Drive/Pazieni/test'
 preprocessing_folder = '/content/drive/My Drive/preproc_data'     
 project_root = '/content/drive/My Drive'                       
-log_root = os.path.join(project_root, 'acdc_logdir')
+log_root = os.path.join(project_root, 'CAD_logdir')
 weights_root = os.path.join(log_root, experiment_name)
 
 # Data settings
