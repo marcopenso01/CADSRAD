fba0c046b6dc70349e0335bae441cb3fbee5cd03
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2021-06-23 18:00:52 +0200

    Update test.py

--- test.py
+++ test.py
@@ -42,7 +42,7 @@ def get_latest_model_checkpoint(my_dir):
           return fname
 
         
-def test_data(input_folder, output_folder, model_path, config, gt_exists=True):
+def test_data(input_folder, output_folder, model_path, config):
   
   logging.warning('EVALUATING ON TEST SET')
   
@@ -60,7 +60,8 @@ def test_data(input_folder, output_folder, model_path, config, gt_exists=True):
   # the following are HDF5 datasets, not numpy arrays
   imgs_test = data['data_train'][()]
   patient_test = data['patient_train'][()]
-  label_test = data['classe_train'][()]
+  if config.gt_exists:
+    label_test = data['classe_train'][()]
 
   if 'data_test' in data:
     logging.warning('ATTENTION: DATA SPLIT INTO TRAIN AND TEST SET...')
@@ -92,55 +93,58 @@ def test_data(input_folder, output_folder, model_path, config, gt_exists=True):
   for ii in range(imgs_test.shape[0]):
     
     img = imgs_test[ii,...]
-    label = label_test[ii,...]
+    if config.gt_exists:
+        label = label_test[ii,...]
     
     if not model.name in 'VGG16, InceptionV3, ResNet50, InceptionResNetV2, EfficientNetB0, EfficientNetB7, ResNet50V2' and config.data_mode == '3D':
       img = img[...,np.newaxis]
     
     prediction = model.predict(np.expand_dims(img, axis=0))
     
-    ytrue.append(label)
+    if config.gt_exists:
+        ytrue.append(label)
     ypred.append(np.argmax(prediction))
     ypred_prob.append(prediction[0].tolist())
   
   ypred_prob = np.array(ypred_prob, dtype=float)
   
-  ytrue_bin = label_binarize(ytrue, classes= range(nclasses))
-  
-  logging.info('Accuracy classification score: %f' % metrics.accuracy_score(ytrue, ypred))
-  logging.info('Balanced accuracy: %f' % metrics.balanced_accuracy_score(ytrue, ypred))
-  logging.info('Precision: %f' % metrics.precision_score(ytrue, ypred, average='micro'))
-  logging.info('Weighted precision: %f' % metrics.precision_score(ytrue, ypred, average='weighted'))
-  logging.info('Recall: %f' % metrics.recall_score(ytrue, ypred, average='micro'))
-  logging.info('Weighted recall: %f' % metrics.recall_score(ytrue, ypred, average='weighted'))
-  logging.info('F1-score: %f' % metrics.f1_score(ytrue, ypred, average='micro'))
-  logging.info('Weighted F1-score: %f' % metrics.f1_score(ytrue, ypred, average='weighted'))
-  
-  logging.info('confusion matrix:')
-  print(metrics.confusion_matrix(ytrue, ypred))
-  
-  plot_file = os.path.join(output_folder, 'roc.png')
-  fpr = dict()
-  tpr = dict()
-  roc_auc = dict()
-  for i in range(nclasses):
-    fpr[i], tpr[i], _ = metrics.roc_curve(ytrue_bin[:, i], ypred_prob[:, i])
-    roc_auc[i] = metrics.auc(fpr[i], tpr[i])
-  
-  colors = ['b', 'g', 'r', 'c', 'm', 'y']
-  for i, color in zip(range(nclasses), colors):
-      plt.plot(fpr[i], tpr[i], color=color, 
-               label='ROC curve of class {0} (area = {1:0.2f})'
-               ''.format(i, roc_auc[i]))
-  plt.plot([0, 1], [0, 1], 'k--')
-  plt.xlim([-0.05, 1.0])
-  plt.ylim([0.0, 1.05])
-  plt.xlabel('False Positive Rate')
-  plt.ylabel('True Positive Rate')
-  plt.title('ROC curve')
-  plt.legend(loc="lower right")
-  plt.savefig(plot_file, dpi=300, bbox_inches='tight')
-  plt.show()
+  if config.gt_exists:
+      ytrue_bin = label_binarize(ytrue, classes= range(nclasses))
+
+      logging.info('Accuracy classification score: %f' % metrics.accuracy_score(ytrue, ypred))
+      logging.info('Balanced accuracy: %f' % metrics.balanced_accuracy_score(ytrue, ypred))
+      logging.info('Precision: %f' % metrics.precision_score(ytrue, ypred, average='micro'))
+      logging.info('Weighted precision: %f' % metrics.precision_score(ytrue, ypred, average='weighted'))
+      logging.info('Recall: %f' % metrics.recall_score(ytrue, ypred, average='micro'))
+      logging.info('Weighted recall: %f' % metrics.recall_score(ytrue, ypred, average='weighted'))
+      logging.info('F1-score: %f' % metrics.f1_score(ytrue, ypred, average='micro'))
+      logging.info('Weighted F1-score: %f' % metrics.f1_score(ytrue, ypred, average='weighted'))
+
+      logging.info('confusion matrix:')
+      print(metrics.confusion_matrix(ytrue, ypred))
+
+      plot_file = os.path.join(output_folder, 'roc.png')
+      fpr = dict()
+      tpr = dict()
+      roc_auc = dict()
+      for i in range(nclasses):
+        fpr[i], tpr[i], _ = metrics.roc_curve(ytrue_bin[:, i], ypred_prob[:, i])
+        roc_auc[i] = metrics.auc(fpr[i], tpr[i])
+
+      colors = ['b', 'g', 'r', 'c', 'm', 'y']
+      for i, color in zip(range(nclasses), colors):
+          plt.plot(fpr[i], tpr[i], color=color, 
+                   label='ROC curve of class {0} (area = {1:0.2f})'
+                   ''.format(i, roc_auc[i]))
+      plt.plot([0, 1], [0, 1], 'k--')
+      plt.xlim([-0.05, 1.0])
+      plt.ylim([0.0, 1.05])
+      plt.xlabel('False Positive Rate')
+      plt.ylabel('True Positive Rate')
+      plt.title('ROC curve')
+      plt.legend(loc="lower right")
+      plt.savefig(plot_file, dpi=300, bbox_inches='tight')
+      plt.show()
   
   out_file = os.path.join(output_folder, 'results.txt')
 
@@ -149,23 +153,32 @@ def test_data(input_folder, output_folder, model_path, config, gt_exists=True):
       text_file.write('\n\n-------------------------------------------------------------------------------------\n')
       text_file.write('CADRADS\n')
       text_file.write('-------------------------------------------------------------------------------------\n\n')
-
-      text_file.write('Accuracy classification score: {:.3f} \n'.format(metrics.accuracy_score(ytrue, ypred)))
-      text_file.write('Balanced accuracy score: {:.3f} \n'.format(metrics.balanced_accuracy_score(ytrue, ypred)))
-      text_file.write('Precision: {:.3f} \n'.format(metrics.precision_score(ytrue, ypred, average='micro')))
-      text_file.write('Weighted precision: {:.3f} \n'.format(metrics.precision_score(ytrue, ypred, average='weighted')))
-      text_file.write('Recall: {:.3f} \n'.format(metrics.recall_score(ytrue, ypred, average='micro')))
-      text_file.write('Weighted recall: {:.3f} \n'.format(metrics.recall_score(ytrue, ypred, average='weighted')))
-      text_file.write('F1-score: {:.3f} \n'.format(metrics.f1_score(ytrue, ypred, average='micro')))
-      text_file.write('Weighted F1-score: {:.3f} \n'.format(metrics.f1_score(ytrue, ypred, average='weighted')))
       
-      text_file.write('----- Prediction ----- \n')
-      text_file.write('patient       real_class       pred_class       probability\n')
-        
-      for ii in range(ypred_prob.shape[0]):
-        
-        text_file.write('{}             {}                {}                {:.3f}\n'.format(patient_test[ii], ytrue[ii].tolist(), ypred[ii].tolist(), np.max(ypred_prob[ii].tolist())))
+      if config.gt_exists:
+          text_file.write('Accuracy classification score: {:.3f} \n'.format(metrics.accuracy_score(ytrue, ypred)))
+          text_file.write('Balanced accuracy score: {:.3f} \n'.format(metrics.balanced_accuracy_score(ytrue, ypred)))
+          text_file.write('Precision: {:.3f} \n'.format(metrics.precision_score(ytrue, ypred, average='micro')))
+          text_file.write('Weighted precision: {:.3f} \n'.format(metrics.precision_score(ytrue, ypred, average='weighted')))
+          text_file.write('Recall: {:.3f} \n'.format(metrics.recall_score(ytrue, ypred, average='micro')))
+          text_file.write('Weighted recall: {:.3f} \n'.format(metrics.recall_score(ytrue, ypred, average='weighted')))
+          text_file.write('F1-score: {:.3f} \n'.format(metrics.f1_score(ytrue, ypred, average='micro')))
+          text_file.write('Weighted F1-score: {:.3f} \n'.format(metrics.f1_score(ytrue, ypred, average='weighted')))
+      
+          text_file.write('----- Prediction ----- \n')
+          text_file.write('patient       real_class       pred_class       probability\n')
 
+          for ii in range(ypred_prob.shape[0]):
+
+            text_file.write('{}             {}                {}                {:.3f}\n'.format(patient_test[ii], ytrue[ii].tolist(), ypred[ii].tolist(), np.max(ypred_prob[ii].tolist())))
+      
+      else:
+          text_file.write('----- Prediction ----- \n')
+          text_file.write('patient       pred_class       probability\n')
+
+          for ii in range(ypred_prob.shape[0]):
+
+            text_file.write('{}             {}                {:.3f}\n'.format(patient_test[ii], ypred[ii].tolist(), np.max(ypred_prob[ii].tolist())))
+      
 
 if __name__ == '__main__':
   
