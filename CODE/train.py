48d9fd3b661494facbf3114971b3d6e6f202ca76
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2022-02-26 22:48:36 +0100

    Update train.py

--- CODE/train.py
+++ CODE/train.py
@@ -65,9 +65,10 @@ def train_test_split(img_data, cad_data, paz_data, ramo_data):
             for j in range(len(coor[0][:])):
                 coor_test.append(coor[0][j])
     
-    train_img = []
-    train_cad = []
-    train_ramo = []
+    tr_img = []
+    tr_cad = []
+    tr_ramo = []
+    tr_paz = []
     test_img = []
     test_cad = []
     test_ramo = []
@@ -78,11 +79,62 @@ def train_test_split(img_data, cad_data, paz_data, ramo_data):
             test_cad.append(cad_data[i])
             test_ramo.append(ramo_data[i])
         else:
-            train_img.append(img_data[i])
-            train_cad.append(cad_data[i])
-            train_ramo.append(ramo_data[i])
+            tr_img.append(img_data[i])
+            tr_cad.append(cad_data[i])
+            tr_ramo.append(ramo_data[i])
+            tr_paz.append(paz_data[i])
+    
+    tr_img = np.asarray(tr_img)
+    tr_cad = np.asarray(tr_cad)
+    tr_ramo = np.asarray(tr_ramo)
+    tr_paz = np.asarray(tr_paz)
+    test_img = np.asarray(test_img)
+    test_cad = np.asarray(test_cad)
+    test_ramo = np.asarray(test_ramo)
+    
+    coor_test = []
+    #validatio-train
+    for cad in np.unique(tr_cad):
+        paz = tr_paz[np.where(tr_cad==cad)]
+        # /10 = 10% for validation
+        var = round(len(np.unique(paz_data[np.where(cad_data==cad)]))/10)
+        random_paz = shuffle(np.unique(paz), random_state=14)
+        test_paz = random_paz[0:var]
+        
+        for i in test_paz:
+            coor = np.where((tr_cad==cad) & (tr_paz==i))
+            for j in range(len(coor[0][:])):
+                coor_test.append(coor[0][j])
+    
+    val_img = []
+    val_cad = []
+    val_ramo = []
+    train_img = []
+    train_cad = []
+    train_ramo = []
+    
+    for i in range(len(tr_paz)):
+        if i in coor_test:
+            val_img.append(tr_img[i])
+            val_cad.append(tr_cad[i])
+            val_ramo.append(tr_ramo[i])
+        else:
+            train_img.append(tr_img[i])
+            train_cad.append(tr_cad[i])
+            train_ramo.append(tr_ramo[i])
+    
+    train_img = np.asarray(train_img)
+    train_cad = np.asarray(train_cad)
+    train_ramo = np.asarray(train_ramo)
+    val_img = np.asarray(val_img)
+    val_cad = np.asarray(val_cad)
+    val_ramo = np.asarray(val_ramo)
+    tr_img = []
+    tr_cad = []
+    tr_paz = []
+    tr_ramo = []
             
-    yield train_img, train_cad, train_ramo, test_img, test_cad, test_ramo
+    yield train_img, train_cad, train_ramo, test_img, test_cad, test_ramo, val_img, val_cad, val_ramo
   
   
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
