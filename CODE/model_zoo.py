b4f39e46b34c39a8e2322737b70c84d6bce52f39
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2022-03-30 16:22:26 +0200

    Update model_zoo.py

--- CODE/model_zoo.py
+++ CODE/model_zoo.py
@@ -90,17 +90,18 @@ def model3(input_size=(256, 256, 3)):
     return model
 
 
-def model4(input_size=(256, 256, 3)):
+def model4(input_size=(256, 256, 3), filters=48, kernel_size1=2, stride_size=2, kernel_size2=5, act='gelu',
+           batch='after', blocks=8):
     """ConvMixer-256/8: https://openreview.net/pdf?id=TVHS5Y4dNvM.
     The hyperparameter values are taken from the paper.
     """
     input = Input(input_size)
     # Extract patch embeddings.
-    x = conv_stem(input, filters=48, patch_size=2)
+    x = conv_stem(input, filters=filters, kernel_size=kernel_size1, strides_size=stride_size, act=act, batch=batch)
 
     # ConvMixer blocks.
-    for _ in range(8):
-        x = conv_mixer_block(x, filters=48, kernel_size=5)
+    for _ in range(blocks):
+        x = conv_mixer_block(x, filters=filters, kernel_size=kernel_size2, act=act, batch=batch)
 
     # Classification block.
     x = GlobalAveragePooling2D()(x)
@@ -109,28 +110,69 @@ def model4(input_size=(256, 256, 3)):
     return model
 
 
+def model5(input_size=(256, 256, 3)):
+    input = Input(input_size)
+    x = Conv2D(48, kernel_size=(3, 3), padding="same", kernel_initializer='he_normal')(input)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = inception_mix(x, 48)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+
+    x = inception_mix(x, 48)
+    x = inception_mix(x, 48)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+
+    x = inception_mix(x, 48)
+    x = inception_mix(x, 48)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+
+    x = GlobalAveragePooling2D()(x)
+    # Add a final sigmoid layer with 1 node for classification output
+    output = Dense(1, activation='sigmoid')(x)
+    model = Model(inputs=input, outputs=output)
+    return model
 
 def custom_gelu(x):
     return 0.5 * x * (1 + tf.tanh(tf.sqrt(2 / np.pi) * (x + 0.044715 * tf.pow(x, 3))))
 
-def activation_block(x):
-    x = custom_gelu(x)
-    return BatchNormalization()(x)
 
-def conv_stem(x, filters: int, patch_size: int):
-    x = Conv2D(filters, kernel_size=patch_size, strides=patch_size)(x)
-    return activation_block(x)
+def activation_block(x, act, batch):
+    if batch == 'before':
+        x = BatchNormalization()(x)
+        if act == 'gelu':
+            return custom_gelu(x)
+        elif act == 'relu':
+            return Activation('relu')(x)
+        else:
+            raise Exception('name activation error')
+    elif batch == 'after':
+        if act == 'gelu':
+            x = custom_gelu(x)
+        elif act == 'relu':
+            x = Activation('relu')(x)
+        else:
+            raise Exception('name activation error')
+        return BatchNormalization()(x)
+    else:
+        raise Exception('name batch error')
 
-def conv_mixer_block(x, filters: int, kernel_size: int):
+
+def conv_stem(x, filters: int, kernel_size: int, strides_size: int, act='gelu', batch='after'):
+    x = Conv2D(filters, kernel_size=kernel_size, strides=strides_size)(x)
+    return activation_block(x, act=act, batch=batch)
+
+
+def conv_mixer_block(x, filters: int, kernel_size: int, act='gelu', batch='after'):
     # Depthwise convolution.
     x0 = x
     x = DepthwiseConv2D(kernel_size=kernel_size, padding="same")(x)
-    x = add([activation_block(x), x0])  # Residual.
+    x = add([activation_block(x, act=act, batch=batch), x0])  # Residual.
 
     # Pointwise convolution.
     x = Conv2D(filters, kernel_size=1)(x)
-    x = activation_block(x)
+    x = activation_block(x, act=act, batch=batch)
     return x
+
 
 def se_block(input_tensor, ratio=16):
     """ Create a channel-wise squeeze-excite block
@@ -230,17 +272,31 @@ def naive_inception_module(layer_in, f1):
     conv2 = Conv2D(f1, (3, 3), padding='same', activation='relu')(conv2)
 
     # concatenate filters, assumes filters/channels last
-    layer_out = Concatenate(axis=1)([conv1, conv2])
+    layer_out = Concatenate(axis=-1)([conv1, conv2])
     return layer_out
+
 
 def residual_block(layer_in, f):
     y = layer_in
     x = Conv2D(f, kernel_size=(3, 3), padding="same", kernel_initializer='he_normal')(layer_in)
     x = BatchNormalization()(x)
-    #x = Activation('relu')(x)
-    #x = Conv2D(f, kernel_size=(3, 3), padding="same", kernel_initializer='he_normal')(x)
-    #x = BatchNormalization()(x)
+    # x = Activation('relu')(x)
+    # x = Conv2D(f, kernel_size=(3, 3), padding="same", kernel_initializer='he_normal')(x)
+    # x = BatchNormalization()(x)
     y = Conv2D(f, kernel_size=(1, 1), padding="same", kernel_initializer='he_normal')(y)
     x = add([x, y])
     x = Activation('relu')(x)
     return x
+
+
+def inception_mix(layer_in, f):
+    x1 = conv_mixer_block(layer_in, filters=f, kernel_size=3, act='gelu', batch='after')
+
+    x2 = conv_mixer_block(layer_in, filters=f, kernel_size=3, act='gelu', batch='after')
+    x2 = conv_mixer_block(x2, filters=f, kernel_size=3, act='gelu', batch='after')
+
+    x3 = conv_mixer_block(layer_in, filters=f, kernel_size=3, act='gelu', batch='after')
+    x3 = conv_mixer_block(x3, filters=f, kernel_size=3, act='gelu', batch='after')
+    x3 = conv_mixer_block(x3, filters=f, kernel_size=3, act='gelu', batch='after')
+
+    return Concatenate(axis=-1)([x1, x2, x3])
